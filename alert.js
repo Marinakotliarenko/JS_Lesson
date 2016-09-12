e0cb90a160754c1388ad227b9423547e662c6b0f
Marina <marinakotliarenko@gmail.com>
2016-09-12 16:27:43 +0300

    2-nd  commit

--- alert.js
+++ alert.js
@@ -8,13 +8,24 @@
 alert( '1');
 alert( '2');
 */
-var a = 3,
-    b = 2;
+var a = 3,  //replace Ctrl+ R or Fn+Shift+F6
+    b = 7;
 var o = {
     name: 'bla'
 };
-a = a * b;
+a = a * b; // Code -> Reformat Code
 b = a / b;
+debugger   //not so usefull as SOURCES breakpoints
 a = a / b;
 
+function test() {
+    var c = 6;
+    return a + b +c;
+}
+test()
 console.log(a, b);
+a = 25;//find usages
+function () {
+    k = 4;
+} //  Code -> Surround with
+// Ctrl+Shift+N -> navigate
---
+++ script2.js
@@ -0,0 +1,8 @@
+/**
+ * Created by marina on 12.09.16.
+ */
+a = 25;//find usages
+function () {
+    k = 4;
+} //  Code -> Surround with
+// Ctrl+Shift+N -> navigate
