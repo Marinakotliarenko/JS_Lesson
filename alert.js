81cda5cfbf0ede6a7d3c54ffb9a78e0db001030e
Marina <marina.kotliarenko@gmail.com>
2016-09-12 14:17:01 +0300

    initial commit

---
+++ alert.js
@@ -0,0 +1,20 @@
+/**
+ * Created by marina on 12.09.16.
+ */
+"use strict"  // отменить его действие нельзя
+//My first SCRIPT
+// alert( ' Я - Javascript');
+/*
+alert( '1');
+alert( '2');
+*/
+var a = 3,
+    b = 2;
+var o = {
+    name: 'bla'
+};
+a = a * b;
+b = a / b;
+a = a / b;
+
+console.log(a, b);
